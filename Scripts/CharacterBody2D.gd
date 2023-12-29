4e2bd4694f4a31456074482a180debebda811774
SexyBird66 <dairo_gamer@outlook.de>
2023-12-29 18:57:07 +0100

    added some stuff

--- Scripts/CharacterBody2D.gd
+++ Scripts/CharacterBody2D.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+enum { run = 1, jump, fall }
 
 
 func _physics_process(delta: float) -> void:
@@ -14,15 +15,33 @@ func _physics_process(delta: float) -> void:
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	chooseAnimation()
 	move_and_slide()
+
+
+func chooseAnimation() -> void:
+	var currentAnim: int
+	if velocity.x != 0:
+		currentAnim = run
+		if velocity.x > 0:
+			$MySprite.
+	match currentAnim:
+		run:
+			$AnimatedSprite2D.play("run")
+		jump:
+			$AnimatedSprite2D.play("jump")
+		fall:
+			$AnimatedSprite2D.play("fall")
+		_:
+			$AnimatedSprite2D.play("idle")
